f21e36e4e97dbea8e945efffc84168789ae7527c
Monika <monika.pietyszuk@gmail.com>
2016-11-28 00:49:08 +0100

    Working CRUD + search + choosing tables

--- src/main/resources/static/js/clients.js
+++ src/main/resources/static/js/clients.js
@@ -1,8 +1,8 @@
 /**
  * Created by Monika on 11/1/2016.
  */
-var crudApp = angular.module('crudApp',['ngMaterial']);
-crudApp.controller("DbController",['$scope','$http', function($scope,$http){
+var crudApp = angular.module('crudApp', ['ngMaterial']);
+crudApp.controller("DbController", ['$scope', '$http', function ($scope, $http) {
 
 // Function to get employee details from the database
     function getInfo() {
@@ -11,42 +11,54 @@ crudApp.controller("DbController",['$scope','$http', function($scope,$http){
         });
     }
 
-    $scope.insertInfo = function(info){
-        $http.post('/clients',{"name":info.name,"surname":info.surname,"email":info.email,"phone":info.phone}).success(function(data){
+    $scope.currentUser = {};
+    $scope.addInfo = function () {
+        console.log("Works");
+        $('#empForm').slideToggle();
+    };
+    
+    $scope.insertInfo = function (info) {
+        $http.post('/clients', {
+            "name": info.name,
+            "surname": info.surname,
+            "email": info.email,
+            "phone": info.phone
+        }).success(function (data) {
             getInfo();
-    // Hide details insertion form
-     //       $('#editForm').css('display', 'none');
         });
     };
 
     $scope.currentUser = {};
-    $scope.addInfo = function() {
-        $('#empForm').slideToggle();
-        $('#editForm').slideUp();
-    };
-
-    $scope.currentUser = {};
-    $scope.editInfo = function(info) {
+    $scope.editInfo = function (info) {
         $scope.currentUser = info;
-        $('#empForm').slideUp();
+        //$('#empForm').slideUp();
         $('#editForm').slideToggle();
     };
 
-    $scope.UpdateInfo = function(info){
-        $http.post('/clients',{"id":info.id,"name":info.name,"surname":info.surname,"phone":info.phone,"email":info.email}).success(function(data){
+    $scope.UpdateInfo = function (info) {
+        $http.post('/clients', {
+            "id": info.id,
+            "name": info.name,
+            "surname": info.surname,
+            "phone": info.phone,
+            "email": info.email
+        }).success(function (data) {
             getInfo();
         });
     };
 
-    $scope.deleteInfo = function(info){
-        $http.delete('/clients/'+info.id,{"id":info.id}).success(function(data){
+    $scope.deleteInfo = function (info) {
+        $http.delete('/clients/' + info.id, {"id": info.id}).success(function (data) {
             getInfo();
-        });
-    $scope.selectChanged = function(){
-        console.log("Changes");
-        if ($scope.tables=="clients"){
-                $scope.getInfo();
-            }
-        };
+        })
+    };
 
-    }}]);
+    $scope.selectChanged = function () {
+        console.log("Changes");
+        if ($scope.tables == "clients") {
+            $('#clients-table').slideToggle();
+            getInfo();
+        }
+    };
+
+}]);
